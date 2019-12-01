3e961d28d4498f2b6e592ac5537daaa2ba823cc5
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2019-12-02 02:05:34 +0700

    chore(tools): git-chglog

--- tools.go
+++ tools.go
@@ -4,6 +4,7 @@ package tools
 
 import (
 	_ "github.com/Songmu/gocredits/cmd/gocredits"
+	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
 	_ "github.com/motemen/gobump/cmd/gobump"
 	_ "golang.org/x/lint/golint"
 )
