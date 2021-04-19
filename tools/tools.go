8b8ec5b1ea54321a3c69f064b422794c5b84edba
Matt (Murahashi Kenichi) <murahashi.kenichi@moneyforward.vn>
2021-04-19 15:56:59 +0700

    chore(tools): re-order

--- tools/tools.go
+++ tools/tools.go
@@ -6,10 +6,10 @@ import (
 	_ "github.com/Songmu/gocredits/cmd/gocredits"
 	_ "github.com/Songmu/goxz/cmd/goxz"
 	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
-	_ "github.com/x-motemen/gobump/cmd/gobump"
 	_ "github.com/sanemat/go-importlist/cmd/import-list"
 	_ "github.com/sanemat/go-xgoinstall/cmd/x-go-install"
 	_ "github.com/tcnksm/ghr"
+	_ "github.com/x-motemen/gobump/cmd/gobump"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
 )
