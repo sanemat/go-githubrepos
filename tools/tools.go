55ef039b6b106f5f128c5351bf163635462e1ca0
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2023-10-07 11:35:31 +0700

    chore(dev-deps): git-chglog

--- tools/tools.go
+++ tools/tools.go
@@ -3,7 +3,6 @@
 package tools
 
 import (
-	_ "github.com/git-chglog/git-chglog/cmd/git-chglog"
 	_ "github.com/sanemat/go-importlist/cmd/import-list"
 	_ "github.com/sanemat/go-xgoinstall/cmd/x-go-install"
 	_ "github.com/tcnksm/ghr"
