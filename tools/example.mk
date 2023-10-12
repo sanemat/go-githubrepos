f5bb2c53f0a4d484525f2273396b50ba658044e0
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2023-10-13 01:14:17 +0700

    update

--- tools/example.mk
+++ tools/example.mk
@@ -1,27 +1,39 @@
 TOOL_VERSION = 1.0.0
 BASE_URL = https://example.com/tool
-TARGET_FILE = path_in_archive_to_file_you_want.ext  # replace with actual path to your file inside the archive
+PROJECT_ROOT = $(PWD)
 
-# By default, use the system's platform. But this can be overridden.
+# Detect platform and architecture.
 PLATFORM ?= $(shell uname -s)
+ARCH ?= $(shell uname -m)
 
+# Define file in archive and binary name based on platform and architecture
 ifeq ($(PLATFORM),Darwin)
-    # macOS
-    ARCHIVE_NAME = $(BASE_URL)-$(TOOL_VERSION)-macOS.tar.gz
-    ARCHIVE_CMD = curl -L $(ARCHIVE_NAME) | tar xzf - -C ./bin/ $(TARGET_FILE)
+    FILE_IN_ARCHIVE = nested_dir/another_dir/path_in_mac_$(ARCH)_archive
+    BIN_NAME = mac_$(ARCH)_command
+    ARCHIVE_NAME = $(BASE_URL)-$(TOOL_VERSION)-macOS-$(ARCH).tar.gz
+    DOWNLOAD_AND_UNPACK = curl -L $(ARCHIVE_NAME) | tar xzf - -C $(PROJECT_ROOT)/bin $(FILE_IN_ARCHIVE)
 endif
+
 ifeq ($(PLATFORM),Linux)
-    # Linux
-    ARCHIVE_NAME = $(BASE_URL)-$(TOOL_VERSION)-linux.tar.gz
-    ARCHIVE_CMD = curl -L $(ARCHIVE_NAME) | tar xzf - -C ./bin/ $(TARGET_FILE)
+    FILE_IN_ARCHIVE = nested_dir/another_dir/path_in_linux_$(ARCH)_archive
+    BIN_NAME = linux_$(ARCH)_command
+    ARCHIVE_NAME = $(BASE_URL)-$(TOOL_VERSION)-linux-$(ARCH).tar.gz
+    DOWNLOAD_AND_UNPACK = curl -L $(ARCHIVE_NAME) | tar xzf - -C $(PROJECT_ROOT)/bin $(FILE_IN_ARCHIVE)
 endif
-ifneq (,$(or $(findstring CYGWIN,$(PLATFORM)), $(findstring MINGW,$(PLATFORM)), $(findstring MSYS,$(PLATFORM))))
-    # Windows
-    ARCHIVE_NAME = $(BASE_URL)-$(TOOL_VERSION)-windows.zip
-    # Note: For zip you'll need a different approach to extract just one file
-    ARCHIVE_CMD = curl -L -o temp.zip $(ARCHIVE_NAME) && unzip temp.zip $(TARGET_FILE) -d ./bin/ && rm temp.zip
+
+ifeq ($(PLATFORM),Windows)
+    FILE_IN_ARCHIVE = nested_dir/another_dir/path_in_windows_$(ARCH)_archive.ext
+    BIN_NAME = windows_$(ARCH)_command.exe
+    ARCHIVE_NAME = $(BASE_URL)-$(TOOL_VERSION)-windows-$(ARCH).zip
+    TEMP_ARCHIVE_FILE = %RANDOM%_$(shell date +%s%N).zip
+    DOWNLOAD_AND_UNPACK = curl -L -o $(TEMP_ARCHIVE_FILE) $(ARCHIVE_NAME) && unzip $(TEMP_ARCHIVE_FILE) $(FILE_IN_ARCHIVE) -d $(PROJECT_ROOT)/bin && rm $(TEMP_ARCHIVE_FILE)
 endif
 
 install:
-	@mkdir -p ./bin
-	$(ARCHIVE_CMD)
+	@if [ -z "$(ARCH)" ] || [ -z "$(DOWNLOAD_AND_UNPACK)" ]; then \
+		go install example.com@$(TOOL_VERSION); \
+	else \
+		mkdir -p $(PROJECT_ROOT)/bin; \
+		$(DOWNLOAD_AND_UNPACK); \
+		mv $(PROJECT_ROOT)/bin/$(FILE_IN_ARCHIVE) $(PROJECT_ROOT)/bin/$(BIN_NAME); \
+	fi
