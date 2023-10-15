b6df25cfa24c9541dcf7aaa6e006bdc91d0ab057
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2023-10-15 18:29:45 +0700

    update command

--- tools/gobump.mk
+++ tools/gobump.mk
@@ -20,33 +20,33 @@ endif
 
 # Platform-specific settings.
 ifeq ($(PLATFORM),Darwin)
-    TARGET_PATH := $(TOOL_VERSION)/gobump_$(TOOL_VERSION)_linux_$(ARCHPATH).tar.gz
-    EXTRACT_DIR := $(MAKEFILE_DIR)$(shell mktemp --dry-run XXXXXX)
-    TEMP_ARCHIVE_FILE := $(MAKEFILE_DIR)$(shell mktemp --dry-run XXXXXX.tar.gz)
-    UNPACK_CMD := tar -xzvf $(TEMP_ARCHIVE_FILE) -C $(EXTRACT_DIR)
-    FILE_IN_ARCHIVE := path_for_mac_in_the_archive
-    BIN_NAME := desired_tool_name_for_mac
+    TARGET_PATH := $(TOOL_VERSION)/gobump_$(TOOL_VERSION)_darwin_$(ARCHPATH).zip
+    EXTRACT_DIR := $(MAKEFILE_DIR)$(shell mktemp --dry-run tempXXXXXX)
+    TEMP_ARCHIVE_FILE := $(MAKEFILE_DIR)$(shell mktemp --dry-run tempXXXXXX.zip)
+    UNPACK_CMD := unzip $(TEMP_ARCHIVE_FILE) -d $(EXTRACT_DIR)
+    FILE_IN_ARCHIVE := gobump_$(TOOL_VERSION)_darwin_$(ARCHPATH)/gobump
+    BIN_NAME := gobump
 else ifeq ($(PLATFORM),Linux)
     TARGET_PATH := $(TOOL_VERSION)/gobump_$(TOOL_VERSION)_linux_$(ARCHPATH).tar.gz
-    EXTRACT_DIR := $(MAKEFILE_DIR)$(shell mktemp --dry-run XXXXXX)
-    TEMP_ARCHIVE_FILE := $(MAKEFILE_DIR)$(shell mktemp --dry-run XXXXXX.tar.gz)
+    EXTRACT_DIR := $(MAKEFILE_DIR)$(shell mktemp --dry-run tempXXXXXX)
+    TEMP_ARCHIVE_FILE := $(MAKEFILE_DIR)$(shell mktemp --dry-run tempXXXXXX.tar.gz)
     UNPACK_CMD := tar -xzvf $(TEMP_ARCHIVE_FILE) -C $(EXTRACT_DIR)
     FILE_IN_ARCHIVE := gobump_$(TOOL_VERSION)_linux_$(ARCHPATH)/gobump
     BIN_NAME := gobump
 else ifneq (,$(or $(findstring CYGWIN,$(PLATFORM)), $(findstring MINGW,$(PLATFORM)), $(findstring MSYS,$(PLATFORM))))
-    TARGET_PATH := $(TOOL_VERSION)/gobump_$(TOOL_VERSION)_linux_$(ARCHPATH).tar.gz
-    EXTRACT_DIR := $(MAKEFILE_DIR)%RANDOM%_$(shell date +%s%N)
-    TEMP_ARCHIVE_FILE := $(MAKEFILE_DIR)%RANDOM%_$(shell date +%s%N).zip
+    TARGET_PATH := $(TOOL_VERSION)/gobump_$(TOOL_VERSION)_windows_$(ARCHPATH).zip
+    EXTRACT_DIR := $(MAKEFILE_DIR)temp%RANDOM%_$(shell date +%s%N)
+    TEMP_ARCHIVE_FILE := $(MAKEFILE_DIR)temp%RANDOM%_$(shell date +%s%N).zip
     UNPACK_CMD := unzip $(TEMP_ARCHIVE_FILE) -d $(EXTRACT_DIR)
-    FILE_IN_ARCHIVE := path_for_windows_in_the_archive
-    BIN_NAME := desired_tool_name_for_windows
+    FILE_IN_ARCHIVE := gobump_$(TOOL_VERSION)_windows_$(ARCHPATH)/gobump.exe
+    BIN_NAME := gobump.exe
 endif
 
 # Set download command.
 DOWNLOAD_CMD := curl -L -o $(TEMP_ARCHIVE_FILE) $(BASE_URL)/$(TARGET_PATH)
 
 install:
-	@if [ -z "$(PLATFORM)" ] || [ -z "$(ARCH)" ]; then \
+	@if [ -z "$(PLATFORM)" ] || [ -z "$(ARCH)" ] || [ -z "$(ARCHPATH)" ]; then \
 		echo "Missing PLATFORM or ARCH, defaulting to 'go install'."; \
 		go install github.com/x-motemen/gobump/cmd/gobump@$(TOOL_VERSION); \
 		exit 0; \
